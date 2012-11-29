1b573bc53affad13649f0ad034fc45c358e5ca43
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-11-29 07:44:07 +0100

    first

---
+++ batiment.sql
@@ -0,0 +1,29 @@
+/*
+	batiments
+*/
+
+BEGIN;
+
+DROP SCHEMA IF EXISTS cadastre;
+CREATE SCHEMA cadastre ;
+
+/* CREATE TABLE */
+DROP TABLE IF EXISTS cadastre.batiment CASCADE;
+CREATE TABLE cadastre.batiment (id serial NOT NULL);
+SELECT setval('cadastre.batiment_id_seq', 100, true);
+COMMENT ON TABLE cadastre.batiment IS 'batiment.';
+
+/* columns */
+ALTER TABLE cadastre.batiment ADD COLUMN genre              varchar(30) DEFAULT '' ;
+ALTER TABLE cadastre.batiment ADD COLUMN design             varchar(60) DEFAULT '' ;
+ALTER TABLE cadastre.batiment ADD COLUMN numero             varchar(12) DEFAULT '' ;
+ALTER TABLE cadastre.batiment ADD COLUMN commune            varchar(20) DEFAULT '' ;
+ALTER TABLE cadastre.batiment ADD COLUMN qualite            varchar(10) DEFAULT '' ;
+
+ALTER TABLE cadastre.batiment ADD COLUMN lbl_x              decimal(12,3)          ;
+ALTER TABLE cadastre.batiment ADD COLUMN lbl_y              decimal(12,3)          ;
+ALTER TABLE cadastre.batiment ADD COLUMN lbl_ori_deg        decimal(6,2)           ;
+
+/* geometry */
+SELECT AddGeometryColumn('cadastre', 'batiment', 'geometry', 21781, 'POLYGON', 2);
+CREATE INDEX batiment_geoidx ON cadastre.batiment USING GIST ( geometry );
